9ad6de84704f977818e6d2358df29d6f05926533
Richard <richardyjkim@gmail.com>
2021-07-03 13:48:15 -0700

    create account form constructed

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,16 +2,18 @@ import React from "react";
 import { CssBaseline } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { withStyles } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Container from '@material-ui/core/Container';
 import wallpaper from "../components/images/wallpaper.jpeg";
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import MuiDialogContent from '@material-ui/core/DialogContent';
-import MuiDialogActions from '@material-ui/core/DialogActions';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
+import { useTheme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import CreateAccount from '../components/CreateAccount';
 import './Home.css';
 
 const useStyles = makeStyles((theme) => ({
@@ -29,15 +31,22 @@ const useStyles = makeStyles((theme) => ({
       color: '#000',
     }
   },
+}));
+
+const styles = (theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
   closeButton: {
     position: 'absolute',
     right: theme.spacing(1),
     top: theme.spacing(1),
     color: theme.palette.grey[500],
   },
-}));
+});
 
-const DialogTitle = withStyles(useStyles)((props) => {
+const DialogTitle = withStyles(styles)((props) => {
   const { children, classes, onClose, ...other } = props;
   return (
     <MuiDialogTitle disableTypography className={classes.root} {...other}>
@@ -57,14 +66,6 @@ const DialogContent = withStyles((theme) => ({
   },
 }))(MuiDialogContent);
 
-const DialogActions = withStyles((theme) => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(1),
-  },
-}))(MuiDialogActions);
-
-
 const Home = () => {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
@@ -76,41 +77,33 @@ const Home = () => {
     setOpen(false);
   };
 
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
     <React.Fragment>
       <CssBaseline />
       <div className="homeContainer">
         <img src={wallpaper} alt="Wallpaper" className="homeImage" />
-        <Container style={{ position: 'realative' }} maxWidth="xl" className="homeText">
+        <Container style={{ position: 'realative' }} maxWidth="lg" className="homeText">
           <h3 className="homeTextName">You know...</h3>
           <h3 className="homeTextTitle">Wagging tails will never disappoint</h3>
           <Button className={classes.button} onClick={handleClickOpen}>Create Account</Button>
-          <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
-            <DialogTitle id="customized-dialog-title" onClose={handleClose}>
-              Modal title
-            </DialogTitle>
-            <DialogContent dividers>
-              <Typography gutterBottom>
-                Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-                in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-              </Typography>
-              <Typography gutterBottom>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-                lacus vel augue laoreet rutrum faucibus dolor auctor.
-              </Typography>
-              <Typography gutterBottom>
-                Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
-                scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
-                auctor fringilla.
-              </Typography>
-            </DialogContent>
-            <DialogActions>
-              <Button autoFocus onClick={handleClose} color="primary">
-                Save changes
-              </Button>
-            </DialogActions>
-          </Dialog>
         </Container>
+        <Dialog
+          onClose={handleClose}
+          aria-labelledby="customized-dialog-title"
+          open={open}
+          maxWidth="sm"
+          fullScreen={fullScreen}
+        >
+          <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+            Create Account
+          </DialogTitle>
+          <DialogContent dividers>
+            <CreateAccount />
+          </DialogContent>
+        </Dialog>
       </div>
     </React.Fragment>
   );
