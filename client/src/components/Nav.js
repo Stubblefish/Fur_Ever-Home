1823b55658c4b1b3a83be01f8e8e5b7f99c4193c
Richard <richardyjkim@gmail.com>
2021-07-03 14:37:21 -0700

    dialog for log in

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -12,6 +13,12 @@ import LockOpenIcon from '@material-ui/icons/LockOpen';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { useTheme } from '@material-ui/core/styles';
+import Dialog from '@material-ui/core/Dialog';
+import CloseIcon from '@material-ui/icons/Close';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -37,11 +44,52 @@ const useStyles = makeStyles((theme) => ({
 
 }));
 
+const styles = (theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+});
+
+const DialogTitle = withStyles(styles)((props) => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+
 export default function ButtonAppBar() {
   const classes = useStyles();
   const theme = useTheme();
   const mobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const [open, setOpen] = React.useState(false);
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div className={classes.root}>
@@ -52,7 +100,20 @@ export default function ButtonAppBar() {
           </Typography>
           {mobile ? <IconButton className={classes.button}><SearchIcon /> </IconButton> : <Button className={classes.button} color="inherit">Search fur family</Button>}
           {mobile ? <IconButton className={classes.button}><FavoriteBorderIcon /> </IconButton> : <Button className={classes.button} color="inherit">Who's ready?</Button>}
-          {mobile ? <IconButton className={classes.button}><LockOpenIcon /> </IconButton> : <Button className={classes.button} color="inherit">log in</Button>}
+          {mobile ? <IconButton className={classes.button} onClick={handleClickOpen}><LockOpenIcon /> </IconButton> : <Button className={classes.button} color="inherit" onClick={handleClickOpen}>log in</Button>}
+          <Dialog
+            onClose={handleClose}
+            aria-labelledby="customized-dialog-title"
+            open={open}
+            maxWidth="xs"
+            fullScreen={mobile}
+          >
+            <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+              Log in
+            </DialogTitle>
+            <DialogContent dividers>
+            </DialogContent>
+          </Dialog>
         </Toolbar>
       </AppBar>
     </div>
