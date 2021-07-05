6a89911adab1ba75ab830b2d02f7b4a9694095fb
Richard <richardyjkim@gmail.com>
2021-07-05 16:53:14 -0700

    login function testing passed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
-import './App.css';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 import Nav from './components/Nav';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import SharedContext from './components/SharedContext';
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql',
+});
 
 function App() {
 
@@ -23,25 +38,29 @@ function App() {
 
   const handleCreateOpen = () => {
     setCreateAccountOpen(true);
-    console.log('create account form opening');
+    console.log('create account form closing');
   };
   const handleCreateClose = () => {
     setCreateAccountOpen(false);
-    console.log('create account form Closing')
+    console.log('create account form open')
   };
 
   return (
-    <div className="App">
+    <ApolloProvider client={client}>
       <CssBaseline />
-      <SharedContext.Provider value={{
-        handleLoginClose, handleLoginOpen, loginOpen, setLoginOpen,
-        createAccountOpen, setCreateAccountOpen, handleCreateOpen, handleCreateClose
-      }}>
-        <Nav />
-        <Home />
-        <Footer />
-      </SharedContext.Provider>
-    </div>
+      <Router>
+        <div>
+          <SharedContext.Provider value={{
+            handleLoginClose, handleLoginOpen, loginOpen, setLoginOpen,
+            createAccountOpen, setCreateAccountOpen, handleCreateOpen, handleCreateClose
+          }}>
+            <Nav />
+            <Home />
+            <Footer />
+          </SharedContext.Provider>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
