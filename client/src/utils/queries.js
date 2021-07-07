f5e8260a0a248fbabba974e14714f7487018b1b5
Stubblefish <george.l.wise@gmail.com>
2021-07-07 16:01:01 -0700

    Removed reducers from src

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,33 +1,33 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const QUERY_PETS = gql`
-query getPets($breed: ID) {
-  pets(breed: $breed) {
-    _id
-    name
-    description
-    price
-    image
-    breed {
+  query getPets($breed: ID) {
+    pets(breed: $breed) {
       _id
+      name
+      description
+      price
+      image
+      breed {
+        _id
+      }
     }
   }
-}
 `;
 
 export const QUERY_ALL_PETS = gql`
-{
-  pets {
-    _id
-    name
-    description
-    image
-    price
-    breed {
+  {
+    pets {
+      _id
       name
+      description
+      image
+      price
+      breed {
+        name
+      }
     }
   }
-}
 `;
 
 export const QUERY_BREEDS = gql`
@@ -40,21 +40,21 @@ export const QUERY_BREEDS = gql`
 `;
 
 export const QUERY_USER = gql`
-{
-  user {
-    firstName
-    lastName
-    adoptions {
-      _id
-      adoptDate
-      pets {
+  {
+    user {
+      firstName
+      lastName
+      adoptions {
         _id
-        name
-        description
-        price
-        image     
+        adoptDate
+        pets {
+          _id
+          name
+          description
+          price
+          image
+        }
       }
     }
   }
-}
 `;
