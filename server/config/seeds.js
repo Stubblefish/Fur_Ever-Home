ecf5bc4fb505163450cd979027e8ad28e0be142b
Stubblefish <george.l.wise@gmail.com>
2021-07-06 18:37:03 -0700

    implemented nicholes db seed

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -31,6 +31,87 @@ db.once("open", async () => {
       price: 200,
       age: 9,
     },
+    {
+      name: "Panda",
+      description:
+        "A French Bulldog that is playful, alert, adaptable, and completely irresistible. She snores, but they are soothing.",
+      breed: breeds[0]._id, 
+      image: "IMG_0308.jpg",
+      price: 1,000,
+      age: 5,
+    },
+    {
+      name: "Shooter",
+      description:
+        "A Bloodhound who is affectionate, gentle, thinks he is a lap dog. Although known for their hunting skills, he likes to take cuddle instead.",
+      breed: breeds[0]._id, 
+      image: "IMG_0998.jpg",
+      price: 500,
+      age: 8,
+    },
+    {
+      name: "Ozzy",
+      description:
+        "A Boxer-Chihuahua mix. Came from an abused home, needs a quiet loving new home. He is crate trained and loves stuffed animals.",
+      breed: breeds[0]._id, 
+      image: "img_6248.jpg",
+      price: 150,
+      age: 1.5,
+    },
+    {
+      name: "Jack",
+      description:
+        "A Saarloos wolfdog mix. She is a wolf dog, but this breed also has a gentle side that makes her a good companion dog. The Saarloos Wolfdog temperament is not for everyone. This breed is best suited to experienced dog owners.",
+      breed: breeds[0]._id, 
+      image: "IMG_5286.jpg",
+      price: 200,
+      age: 9,
+    },
+    {
+      name: "Dude",
+      description:
+        "A Cairn Terrier mix to the max. He has lots of energy and loves to herd his brothers or sisters while walking. Though he gets along with other dogs, you'll usually find him making new friends with other humans.",
+      breed: breeds[0]._id, 
+      image: "IMG_3503.jpg",
+      price: 175,
+      age: 11,
+    },
+    {
+      name: "Bruce",
+      description:
+        "A pure bred Labrador Retriever. He came from a fine line of duck hunting dogs, but he is terrified of ducks. He is a wonderful family dog and loves kids.",
+      breed: breeds[0]._id, 
+      image: "img_6988.jpg",
+      price: 350,
+      age: 6,
+    },
+    {
+      name: "Canyon",
+      description:
+        "An Australian Cattle Dog loves the outdoors. He loves long walks/hikes, kayaking, and playing all around. Family dog all the way.",
+      breed: breeds[0]._id, 
+      image: "IMG_9292.jpg",
+      price: 200,
+      age: 3,
+    },
+    {
+      name: "Jimmy",
+      description:
+        "A dachshund mix with the mindset of a Bullmastiff. Grumpy old man who loves children! Very protective of his family, especially when the mailslot opens.",
+      breed: breeds[0]._id, 
+      image: "IMG_2638.jpg",
+      price: 125,
+      age: 9,
+    },
+    {
+      name: "Indie",
+      description:
+        "A pure bred Border Collie. She is a highly intelligent, extremely energetic, acrobatic and athletic dog. Can play ball for hours on end.",
+      breed: breeds[0]._id, 
+      image: "IMG_3424.jpg",
+      price: 350,
+      age: 4,
+    },
   ]);
 
   console.log("Pet seeded!");
